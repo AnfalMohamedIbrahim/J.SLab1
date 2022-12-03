fc470e920b9a312ce196cece311e4dbe18c3414d
AnfalMohamedIbrahim <anfal.mohamed38@gmail.com>
2022-12-03 23:46:41 +0200

    D2Bonus

---
+++ D2Bonus.js
@@ -0,0 +1,23 @@
+/**
+ * BONUS
+ */
+
+ var a = prompt("enter number!");
+ a= parseInt(a);
+//  console.log(a);
+ var b = prompt("enter number!");
+ b= parseInt(b);
+//  console.log(b);
+ var c = prompt("enter number!");
+ c= parseInt(c);
+//  console.log(c);
+ var trueresult = b*c+a;
+//  console.log(trueresult); 
+ var d = prompt("enter number!");
+ d= parseInt(d);
+//  console.log(d);
+ if(trueresult == d){
+     alert("yes");
+ }else{
+     alert("no");
+ }
